1213b509e7be1f98583ab45f223b5cf53200e61a
Ahmed Moussa <ahmedmoussa@ahmeds-air.mobile.asu.edu>
2017-01-17 16:36:50 -0700

    what i got so far
    parsing line inputs with strtok()

--- ass1/ass1/main.c
+++ ass1/ass1/main.c
@@ -7,9 +7,52 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 
+int parseLine(char* line){
+    char* everything[10];
+    char* element;
+    printf ("Splitting line \"%s\" into elements:\n",line);
+    element = strtok(line," ");
+    everything[0] = element;
+    int i = 1;
+    while (element != NULL && i<10)
+    {
+        printf ("%s\n",element);
+        element = strtok (NULL, " ");
+        everything[i] = element;
+        i++;
+    }
+    for(int j = i-1; j>=0; j--){
+        printf ("element number %d is %s\n", j, everything[j]);
+    }
+    return 0;
+};
+
+int getInput(){
+    char* input;                                                    //initiating string for input to be placed in
+    int numOfBytes = 1;                                             //number of bytes for input string IDUHTW!!!
+    if(getline(&input, &numOfBytes, stdin)){                         //Try the getline, see if it works
+        printf("arright, the getline went through\n");
+        printf("here's the line: %s",input);
+        parseLine(input);
+        return 1;
+    }
+    else{
+        printf("getline didn't go through apparently :/\n");
+    }
+    return 0;
+};
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
     printf("Hello, World!\n");
+    
+    if(getInput()){                                                 //Try the getInput, see if it works
+        printf("back from getInput, it worked!\n");
+    }
+    else{
+        printf("someone fucked up\n");
+    }
+    
     return 0;
 }
