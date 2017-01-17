61da41eec42d2e2c8235051e26a8b48d5695109e
Ahmed Moussa <ahmedmoussa@ahmeds-air.mobile.asu.edu>
2017-01-17 16:34:47 -0700

    initial commit
    just new Xcode project

---
+++ ass1/ass1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ass1
+//
+//  Created by Ahmed Moussa on 1/17/17.
+//  Copyright © 2017 Ahmed Moussa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
