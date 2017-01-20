57da62fb3ca345fc8aaafe2f4950504d460a0a5a
Kendall Hickie <khickie@asu.edu>
2017-01-20 14:43:47 -0700

    just added new project
    new project _v2

---
+++ ass1_v2/ass1_v2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ass1_v2
+//
+//  Created by Kendall Hickie on 1/20/17.
+//  Copyright © 2017 Kendall Hickie. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
